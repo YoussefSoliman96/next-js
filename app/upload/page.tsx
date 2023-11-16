f10fdca9616999af367d23a14e7c316895e2fe4d
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-16 03:31:58 +0200

    Show uploaded images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,38 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="jpih4m0j">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <div>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          alt="Google"
+          width={270}
+          height={180}
+        ></CldImage>
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="jpih4m0j"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </div>
   );
 };
 
