1314dd864c93f46b8d3b48341fd68ee96d509e7b
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-18 19:56:56 +0200

    Protect routes

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+// import middleware from 'next-auth/middleware'
+export { default } from 'next-auth/middleware'
+
+// export default middleware
+
+export const config = {
+    matcher: ['/dashboard/:path*']
+}
