2eb35816ed4316883f957e8fe9e46b62438f3f21
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-20 00:21:11 +0200

    Use custom fonts (poppins)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,22 @@
-import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import NavBar from "./NavBar";
-import { SessionProvider } from "next-auth/react";
-import AuthProvider from "./auth/Provider";
-import Script from "next/script";
+import { Inter, Roboto } from "next/font/google";
+import localFont from "next/font/local";
 import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
+import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
+
+const poppins = localFont({
+  src: "../public/fonts/poppins-regular-webfont.woff2",
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <GoogleAnalyticsScript />
-      <body className={inter.className}>
+      <body className={poppins.variable}>
         <AuthProvider>
           <NavBar />
           <main className="p-5 ">{children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import image from "@/public/images/image.png";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <main className="relative h-screen">
-      <Image
+    <main>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard></ProductCard>
+      {/* <Image
         src="https://bit.ly/react-cover"
         alt="photo"
         fill
@@ -17,7 +19,7 @@ export default async function Home() {
         sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
         quality={100}
         priority
-      />
+      /> */}
     </main>
   );
 }
